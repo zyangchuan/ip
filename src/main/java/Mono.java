866739e8b7549fe281eb7cdcf09939aad9b05302
zyangchuan <zyangchuan1@gmail.com>
2026-08-19 17:37:08 +0800

    Added echo user input feature

--- src/main/java/Mono.java
+++ src/main/java/Mono.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Mono {
     public static void main(String[] args) {
         String banner = "███╗   ███╗ ██████╗ ███╗   ██╗ ██████╗ \n" +
@@ -6,16 +8,40 @@ public class Mono {
                         "██║╚██╔╝██║██║   ██║██║╚██╗██║██║   ██║\n" +
                         "██║ ╚═╝ ██║╚██████╔╝██║ ╚████║╚██████╔╝\n" +
                         "╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ";
-
-        String exitMessage = String.format(
+        String greetingMessage = String.format(
                         "____________________________________________________________\n" +
                         "%s\n" +
                         "Hello! I'm Mono.\n" +
                         "What can I do for you?\n" +
-                        "____________________________________________________________\n" +
-                        "Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n", banner);
 
-        System.out.println(exitMessage);
+        String exitMessage =
+                        "____________________________________________________________\n"+
+                        "Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n";
+
+        System.out.println(greetingMessage);
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            while (true) {
+                String input = "";
+                input = scanner.nextLine();
+
+                if (input.equals("bye")) {
+                    System.out.println(exitMessage);
+                    break;
+                }
+
+                System.out.println(
+                                "____________________________________________________________\n" +
+                                input + "\n" +
+                                "____________________________________________________________\n"
+                );
+            }
+        } finally {
+            scanner.close();
+        }
     }
 }
